53765b9d375220aa34bc16b78d6f5149f8de2cba
NewYoung <657851574@qq.com>
2019-08-20 18:11:09 +0800

    修改

--- index.js
+++ index.js
@@ -14,13 +14,34 @@
     window.SimplePullLoading = factory(window.Prefix)
   }
 })(function (Prefix) {
+  //兼容性处理
   var transformProperty = Prefix.prefix('transform')
+  var transitionDurationPro = Prefix.prefix('transitionDuration');
+
+  function whichTransitionEvent() {
+    var t;
+    var el = document.createElement('div');
+    var transitions = {
+      'transition': 'transitionend',
+      'OTransition': 'oTransitionEnd',
+      'MozTransition': 'transitionend',
+      'WebkitTransition': 'webkitTransitionEnd',
+      'MsTransition': 'msTransitionEnd'
+    };
+    for (t in transitions) {
+      if (el.style[t] !== undefined) {
+        return transitions[t];
+      }
+    }
+  }
+  var transitionEndEvent = whichTransitionEvent();
 
   /**
    * tyle 类型 down-下拉刷新 up-上拉刷新
    * $outer 外层容器
    * $inner 内层容器
    * $list  滚动列表
+   * loadingFunc 加载数据回调
    */
   function SimplePullLoading(config) {
     if (!config || typeof config !== 'object') {
@@ -33,6 +54,10 @@
     this.$list = config.$list;
     this.type = config.type || 'down';
     this.backgroundColor = config.backgroundColor;
+    this.loadingFunc = config.loadingFunc || function () {};
+
+    this._isLoading = false; //是否在加载
+
     this.default();
     this.event();
   }
@@ -43,20 +68,23 @@
      * 20-60 tip
      * 60-100 loading、success 
      */
+    this._scrollTop = 0;
     this._moveY = 0;
     this._maxMoveY = 100;
     this._pullMinHeight = 20;
     this._loadingMinHeight = 60;
 
     this._successClass = 'sim-success';
+    this._loosenClass = 'sim-loosen';
     this._tipClass = 'sim-tip';
     this._loadingClass = 'sim-loading';
+    this._pullDisplay = 'flex';
 
     this.$pull = document.createElement('div');
     this.$pull.classList.add('sim-pull-loading');
 
     var html = '';
-    html += '<div class="pull-tip">下拉刷新</div>';
+    html += '<div class="pull-tip"></div>';
     html += '<div class="pull-loading">';
     html += '<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';
     html += '<p>正在加载</p>';
@@ -64,7 +92,7 @@
     html += '<div class="pull-success">刷新成功</div>'
 
     this.$pull.innerHTML = html;
-    this.$pull.style.display = 'none'; //刚开始隐藏
+    this.$pull.style.display = 'none'; //隐藏
     this.$outer.appendChild(this.$pull);
 
     switch (this.type) {
@@ -82,48 +110,79 @@
   //事件监听
   SimplePullLoading.prototype.event = function () {
     var self = this;
+
     var movePoint;
     self.$list.addEventListener('touchmove', function (e) {
-      e.preventDefault();
-      var point = e.touches[0];
-      if (!movePoint) {
-        movePoint = {
-          x: point.clintX,
-          y: point.clientY
-        };
-        self.$pull.style.display = 'block'; //显示
-      } else {
-        var moveY = point.clientY - movePoint.y;
+      if (!self._isLoading) {
+        var point = e.touches[0];
+        if (!movePoint) {
+          movePoint = {
+            x: point.clintX,
+            y: point.clientY
+          };
+          self.showPull(); //显示
+        } else {
+          var moveY = point.clientY - movePoint.y;
 
-        if (self.type == 'down' && moveY > 0) { //下拉刷新
-          self._moveY += moveY * (self._maxMoveY - self._moveY) / self
-            ._maxMoveY;
-          self.$list.style.transform = 'translateY(' + self._moveY + 'px)';
+          if (self.type == 'down' && moveY > 0) { //下拉刷新
+            self._moveY += moveY * (self._maxMoveY - self._moveY) / self
+              ._maxMoveY;
+            self.moving();
+          }
+
+          if (self._moveY >= self._pullMinHeight) {
+            self.showTip();
+          }
+
+          if (self._moveY >= self._loadingMinHeight) {
+            self.showLoosen();
+          }
+
+          movePoint.x = point.clientX;
+          movePoint.y = point.clientY;
         }
-
-        if (self._moveY >= self._pullMinHeight) {
-          self.showTip();
-        }
-
-        if (self._moveY >= self._loadingMinHeight) {
-          self.showLoading();
-        }
-
-        movePoint.x = point.clientX;
-        movePoint.y = point.clientY;
       }
     })
+
+    self.$list.addEventListener('touchend', function () {
+      if (self._moveY > self._loadingMinHeight) {
+        self._isLoading = true;
+        self._moveY = self._loadingMinHeight;
+        self.showLoading();
+        self.loadingFunc();
+        self.moving()
+      } else {
+        self.hidePull();
+      }
+      movePoint = null;
+    })
+  }
+
+  //移动
+  SimplePullLoading.prototype.moving = function () {
+    this.$list.style[transformProperty] = 'translateY(' + (this._moveY + this._scrollTop) + 'px)';
+    this.$pull.style.height = this._moveY + 'px';
   }
 
   //显示提示
   SimplePullLoading.prototype.showTip = function () {
+    this.$pull.classList.remove(this._loosenClass)
     this.$pull.classList.remove(this._successClass)
     this.$pull.classList.remove(this._loadingClass)
     this.$pull.classList.add(this._tipClass)
   }
 
+  //显示提示
+  SimplePullLoading.prototype.showLoosen = function () {
+    this.$pull.classList.remove(this._tipClass)
+    this.$pull.classList.remove(this._successClass)
+    this.$pull.classList.remove(this._loadingClass)
+    this.$pull.classList.add(this._loosenClass)
+  }
+
   //显示加载动画
   SimplePullLoading.prototype.showLoading = function () {
+    this.$pull.classList.remove(this._loosenClass)
     this.$pull.classList.remove(this._successClass)
     this.$pull.classList.remove(this._tipClass)
     this.$pull.classList.add(this._loadingClass)
@@ -131,9 +190,41 @@
 
   //显示成功
   SimplePullLoading.prototype.showSuccess = function () {
+    var self = this;
+    self.$pull.classList.remove(this._loosenClass)
+    self.$pull.classList.remove(this._tipClass)
+    self.$pull.classList.remove(this._loadingClass)
+    self.$pull.classList.add(this._successClass)
+    self.$pull.style[transitionDurationPro] = '.2s';
+    self.$list.style[transitionDurationPro] = '.2s';
+    self._moveY = 0;
+    self.moving();
+
+    self.$pull.addEventListener(transitionEndEvent, function () {
+      self.hidePull();
+    })
+  }
+
+  //显示拉动刷新组件
+  SimplePullLoading.prototype.showPull = function () {
+    this.$pull.style.display = this._pullDisplay;
+    this._scrollTop = this.$inner.scrollTop;
+    this.$inner.style.overflowY = 'hidden';
+    this.$list.style[transformProperty] = 'translateY(-' + this._scrollTop + 'px)';
+  }
+
+  //隐藏拉动刷新组件
+  SimplePullLoading.prototype.hidePull = function () {
+    this._isLoading = false;
+    this._moveY = 0;
+    this.$pull.style.display = 'none';
+    this.$pull.style[transitionDurationPro] = '0s';
+    this.$list.style[transitionDurationPro] = '0s';
+    this.$pull.classList.remove(this._loosenClass)
+    this.$pull.classList.remove(this._successClass)
     this.$pull.classList.remove(this._tipClass)
     this.$pull.classList.remove(this._loadingClass)
-    this.$pull.classList.add(this._successClass)
+    this.moving();
   }
 
   return SimplePullLoading
